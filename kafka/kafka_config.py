82ab841153eba7d781de6a86aea1b261a1c61321
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-08-16 10:28:29 +0530

    update kafka_config.py

--- kafka/kafka_config.py
+++ kafka/kafka_config.py
@@ -3,11 +3,11 @@ import os
 SECURITY_PROTOCOL = "SASL_SSL"
 SSL_MACHENISM = "PLAIN"
 API_KEY = "M7AF5MHWTEHMM42X"
-ENDPOINT_SCHEMA_URL = "https://psrc-4nrnd.us-central1.gcp.confluent.cloud"
-API_SECRET_KEY = "8VvGLYI2keS9ZFGFNaGfq7uUk0oWLbkxFr9lSzkbAca9lfxDPPjTKk/1o+NrSsJ4"
-BOOTSTRAP_SERVER = "pkc-6ojv2.us-west4.gcp.confluent.cloud:9092"
-SCHEMA_REGISTRY_API_KEY = "3NMBHCKPYMUP6EPE"
-SCHEMA_REGISTRY_API_SECRET = "UXlUK633aEEp3aX2jjienBc0AfnoF/Fe33AaWRaKvW99CyfkT1nEakg8WA5kfQHu"
+ENDPOINT_SCHEMA_URL = "https://poi-4qwer.us-central1.gcp.confluent.cloud"
+API_SECRET_KEY = "8VvGLYI2keS9ZFGFNaGfq7uUk0oWLbkxFr9lSzkbAca9lfxDPPjTKk/234dfghl"
+BOOTSTRAP_SERVER = "plm-fgbhn.us-west4.gcp.confluent.cloud:9092"
+SCHEMA_REGISTRY_API_KEY = "3NMBHCDFG67U8I"
+SCHEMA_REGISTRY_API_SECRET = "UXlUK633aEEp3aX2jjienBc0AfnoF/Fe33AaWRaCVGHTYU71nEakg8WA5kfQHu"
 
 
 def sasl_conf():
